cf856982e0e9a4e20eee558baf1ef4f239b4493f
Daniel <danielneto.dev@gmail.com>
2021-08-30 10:13:32 -0300

    initial commit

---
+++ src/main/java/com/dansalesdev/springapi/ApiSprngApplication.java
@@ -0,0 +1,13 @@
+package com.dansalesdev.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSprngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSprngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dansalesdev/springapi/ApiSprngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dansalesdev.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSprngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
